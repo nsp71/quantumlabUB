7a2661777fefa381811f6d2d6114bbf0fffa37e9
nsp71 <49080532+nsp71@users.noreply.github.com>
2024-05-09 12:32:07 +0200

    Update percolacio_quadrat.py
    estudi estadístic i determinació de p_c amb incertesa

--- percolació/percolacio_quadrat.py
+++ percolació/percolacio_quadrat.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 from collections import deque
+from scipy import stats
+import math as math
+
 
 
 
@@ -22,29 +25,12 @@ def matriu_quadrat(n,p):
 
 ############################################################################################################################
 
-#nomes funciona fins matrius de mida n = 60 :(
-#IN: 1) Matriu nxn. 2) Posició i, j (ha de tenir una valor = 1)
-#OUT: Llista de les posicions del cluster que comença a i,j
+#prova
 
-def cluster_funcio(matriu, i, j, n, visitat):
-    max_depth = 10
-    cluster_vec = []
+def matriu_aux(n):
+    return np.random.randint(2, size=(n,n))
 
-    # Funció per buscar veins de veins etc recursiva (potser no és el més eficient) -> peta amb matrius de mida >= 60
-    def veins_recursius(x, y, depth):
-        if depth >= max_depth or visitat[x, y]:
-            return
-        visitat[x, y] = True
-        cluster_vec.append((x, y))
 
-        # Explorar los vecinos
-        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < len(matriu) and 0 <= ny < len(matriu[0]) and matriu[nx, ny] == 1 and not visitat[nx, ny]:
-                veins_recursius(nx, ny,0)
-
-    veins_recursius(i, j, 0)
-    return cluster_vec
 
 ###########################################################################################################################
 
@@ -62,16 +48,14 @@ def bfs(matriu, inici, visitat):
     while queue:
         row, col = queue.popleft()
         visitat[row, col] = True
-
-        if matriu[row,col] == 1:
-            vertex_actius.append((row,col))
+        vertex_actius.append((row, col))
 
         # Obtener vecinos válidos no visitados
         for neighbor_row, neighbor_col in [(row-1, col), (row+1, col), (row, col-1), (row, col+1)]:
             #amb la condició 0 <= ens assegurem que estem dins dels límits de la matriu.
             if 0 <= neighbor_row < len(matriu) and 0 <= neighbor_col < len(matriu[0]) and not visitat[neighbor_row, neighbor_col] and matriu[neighbor_row, neighbor_col] == 1:
                 visitat[neighbor_row, neighbor_col] = True
-                vertex_actius.append((neighbor_row,neighbor_col))
+                #vertex_actius.append((neighbor_row,neighbor_col))
                 queue.append((neighbor_row, neighbor_col))
 
     return vertex_actius
@@ -95,6 +79,7 @@ def busca_clusters(matriu):
         for j in range(n):
             if matriu[i, j] == 1 and not visitat[i, j]:
                 llista_clusters.append(bfs(matriu, (i, j), visitat))
+                visitat[i, j] = True
 
     return llista_clusters
 
@@ -123,8 +108,9 @@ def pintar_clusters(matriu,vertex_actius):
 def percola(matriu, cluster_per):
 
     clusters = busca_clusters(matriu)
+    print(clusters)
     # # de clusters del sistema
-    n = len(clusters)
+    n = len(matriu[0]) #variable mal inicialitzada, per això no acabava de funcionar el programa
     #ens servirà per identificar el cluster que percola. Els enters no poden canviar si són passats com arguments de la funció
     perc_n = 0
 
@@ -138,11 +124,13 @@ def percola(matriu, cluster_per):
     right = False
 
     for k in range(len(clusters)):
+        top = False
+        bottom = False
         for i in range(len(clusters[k])):
             if clusters[k][i][0] == 0:
                 top = True
                 perc_n = k
-            if clusters[k][i][1] == n - 1:
+            if clusters[k][i][0] == n - 1:
                 bottom = True
                 perc_n = k
             if clusters[k][i][0] == n - 1:
@@ -151,11 +139,15 @@ def percola(matriu, cluster_per):
             if clusters[k][i][1] == n - 1:
                 right = True
                 perc_n = k
+        if (top and bottom):
+            break
 
         #comprovem totes les possibilitats de percolació
 
-    if ((top and bottom) or (top and right) or (top and left) or (left and right) or (bottom and right) or (bottom and left)):
+    #if ((top and bottom) or (top and right) or (top and left) or (left and right) or (bottom and right) or (bottom and left)):
         #clusters_per = cluster_percolat.append(clusters[perc_n])
+    if (top and bottom):
+        print('ha percolat!')
         perc_n += 1
     else:
         perc_n = -1
@@ -194,14 +186,7 @@ def biggest_cluster_frac(matriu):
     clusters = busca_clusters(matriu)
     max_cluster_size = 0
 
-    #Eliminen les repeticions de clusters. Altrament serà molt complicat trobar la mida del cluster més gran
-    clusters_sin_repeticiones = []
     for cluster in clusters:
-        cluster_sin_repeticiones = list(
-            set(cluster))  # Convertimos a conjunto para eliminar repeticiones y luego a lista
-        clusters_sin_repeticiones.append(cluster_sin_repeticiones)
-
-    for cluster in clusters_sin_repeticiones:
         cluster_size = len(cluster)
         #actualitzem la mida del cluster més gran fins el moment
         if cluster_size > max_cluster_size:
@@ -209,9 +194,93 @@ def biggest_cluster_frac(matriu):
 
     return max_cluster_size/ver_total
 
-
-
 ###########################################################################################################################
+
+#IN: 1) Matriu de dimensions nxn
+#OUT: Fracció del cluster que ha percolat (en cas d'haver percolació). Altrament retorna 0.
+
+def fracc_cluster_per(matriu):
+
+    n_total = len(matriu)*len(matriu[0])
+    aux = []
+    if percola(matriu, aux) == -1:
+        return 0
+    else:
+        clusters = busca_clusters(matriu)[percola(matriu, aux) - 1]
+        n_clusters_per = len(clusters)
+        return n_clusters_per/n_total
+
+############################################################################################################################
+
+#IN: 1) Iteracions de la simulació. 2) Mida n de les matrius
+#OUT: 2) Vector de dimensions 'interacions' amb un conjunt de punts crítics
+#per matrius de mida nxn
+def vector_pc(iteracions,n):
+    x = np.zeros(iteracions)
+    for i in range(iteracions):
+        p = 0
+        matriu = matriu_quadrat(n, p)
+        while percola(matriu,[]) == -1:
+            matriu = matriu_quadrat(n, p)
+            p += 0.01
+            x[i] = p
+    return x
+
+############################################################################################################################
+############################################################################################################################
+
+                                            # ESTADÍSTICA:
+
+#IN: 1) Vector de valors. 2) n, Número d'iteracions
+#OUT: Desviació estàndard
+def standard_dev(n,valors):
+    suma = 0
+    mean = mean_value(valors)
+    for i in range(n):
+        suma += (valors[i] - mean)**2
+    return np.sqrt(suma/(n-1))
+
+############################################################################################################################
+
+#IN: 1) Vector values
+#OUT: Mitjana de values (sumar els seus elements i dividr-los per la mida de values
+def mean_value(values):
+    suma = sum(values)
+    return suma/len(values)
+
+############################################################################################################################
+
+#IN: 1) Vector de valors. 2) n, Número d'iteracions
+#OUT: Incertesa de la mesura
+def incertesa(n,valors):
+    desv_est = standard_dev(n,valors)
+    return desv_est/np.sqrt(n)
+
+############################################################################################################################
+
+#IN: 1) Vector de valors. 2) n, Número d'iteracions
+#OUT: Incertesa relativa
+def incertesa_relativa(n,valors):
+    return (incertesa(n,valors)/mean_value(valors))
+
+############################################################################################################################
+
+def U_n(valors,iteracions):
+#Aplicar la fórmula 7 del paper:
+#Investigations into the Influence of Matrix Dimensions and Number of Iterations on the Percolation Phenomenon for Direct Current
+    mean = mean_value(valors)
+    standard_dev(iteracions,valors)
+    suma = 0
+    for i in range(iteracions):
+        suma += (x[i]+mean)
+    numerador = (1/iteracions)*suma
+    denominador = standard_dev(iteracions,valors)/np.sqrt(iteracions)
+    return numerador/denominador
+
+############################################################################################################################
+############################################################################################################################
+
+                                                # GRÀFIQUES:
 
 #Gràfica del punt crític de percolació en funció de les dimensions de la matriu quadrada pel cas site percolation
 def g1():
@@ -255,15 +324,15 @@ def g1():
 
 #Gràfica de la fracció de vèrtexs connectats en funció de la probabilitat de percolació pel cas site percolation
 def g2():
-    probabilities = np.arange(0.1, 1.0, 0.01)
+    probabilities = np.arange(0.1, 1.0, 0.1)
     num_trials = 100  # Número de matrices a generar para cada probabilidad
     avg_fracs = []  # Almacenar el promedio de las fracciones de vértices conectados
 
     for p in probabilities:
         frac_sum = 0
         for _ in range(num_trials):
-            matrix = matriu_quadrat(500, p)
-            frac_sum += biggest_cluster_frac(matrix)  #fracció de cluster més gran
+            matrix = matriu_quadrat(50, p)
+            frac_sum += biggest_cluster_frac(matrix)  #fracció de cluster més gran ESTO ESTÁ MAL, DEBERÍA SER LA FRACCIÓN DE CLUSTERS CONECTADOS INDEPENDIENTEMENTE DE SU TAMAÑO
         avg_frac = frac_sum / num_trials  #mitjana de cluster de mida més gran per matriu de mida n i p
         avg_fracs.append(avg_frac)
 
@@ -276,23 +345,113 @@ def g2():
     plt.show()
 
 ############################################################################################################################
+
+# Per dibuixar el mapa
+def heatmap(n,p,iteracions):
+    matriu_diagrama = np.zeros((n, n), dtype=int)
+    for k in range(iteracions):
+        matriu = matriu_quadrat(n, p)
+        for i in range(len(matriu)):
+            for j in range(len(matriu[0])):
+                if (matriu[i][j]) == 1:
+                    matriu_diagrama[(n - 1) - i][j] += 1
+
+    plt.imshow(matriu_diagrama, cmap='viridis', interpolation=None)
+    plt.colorbar()
+    plt.title('Mapa de distribució aleatòria dins de les matrius')
+    plt.xlabel('Columnes (0-55)')
+    plt.ylabel('Files (0-55)')
+    plt.gca().invert_yaxis()
+    plt.show()
+
+############################################################################################################################
+
+def figure5(iteracions):
+    ps = [0.2, 0.6, 0.9]
+    ns = [55, 101, 155]
+    for p in ps:
+        for n in ns:
+            vector = np.zeros(n)
+            centre = (n - 1) // 2
+            for k in range(iteracions):
+                matriu = matriu_quadrat(n, p)
+                for i in range(n):
+                    if (matriu[i][centre] == 1 or matriu[centre][i] == 1):
+                        vector[i] += 1
+
+            plt.scatter(range(n), vector, label=f'n={n}, p={p}')  # Etiquetar cada conjunto de datos con n y p
+
+    plt.xlabel('Coordinates of nodes, a.u.')
+    plt.ylabel('Number of points, a.u.')
+    plt.title('Evolution of vectors for different n and p')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+############################################################################################################################
+
+def histograma(n,iteracions):
+    #primer de tot anem a obtenir un vector de dimensió n on anirem
+    #guardant els valors de percolació de cada iteració n
+    x = vector_pc(iteracions,n)
+    n_bins = 7
+    hist = np.histogram(x,bins=n_bins,density=True)
+    #hist_dist = stats.rv_histogram(histogram=hist,density=True)
+    hist_dist = stats.rv_histogram(histogram=hist, density=True)
+    plt.hist(x,bins=n_bins,edgecolor="white",density=True,label="Histogram")
+    eix_x = np.linspace(min(x),max(x),n_bins)
+    plt.plot(eix_x,hist_dist.pdf(eix_x),label="PDF")
+
+    plt.xlabel('Punts crítics')
+    plt.ylabel('Densitat')
+    plt.title('Histograma de Freqüències')
+
+    plt.grid(True)
+    plt.show()
+
+############################################################################################################################
 ############################################################################################################################
 
                                 # CÓDIGO PRINCIPAL:
 
-#Gràfica del punt crític de percolació en funció de les dimensions de la matriu quadrada pel cas site percolation
+#condicions incials
+n = 55
+p = 0.7
+iteracions = 5 * 10 ** 2
+
+# (1): Gràfica del punt crític de percolació en funció de les dimensions de la matriu quadrada pel cas site percolation
 #g1()
 
-#Gràfica de la fracció de vèrtexs connectats en funció de la probabilitat de percolació
-g2()
+# (2): Gràfica de la fracció de vèrtexs connectats en funció de la probabilitat de percolació
+#g2()
+
+# (3): mapa que mostra la distribució aleatòria de generació de números dins de la matrius (per diferents mides i p's)
+#al simular diferents mides i p's no s'ha observat cap patró en cap cas. Per tant, les distribucions aleatòries
+# es generen correctament. (Figure 2, 3 i 4)
+#heatmap()
+
+# (4): 1's al llarg de les línies horitzontals i verticals que passen pel centre
+#figure5(iteracions)
+
+
+
+# (6) Histograma
+histograma(n,iteracions)
+
+#calcul punt crític
+x = vector_pc(iteracions,n)
+mean = mean_value(x)
+inc = incertesa(iteracions,x)
+print('p_c = ', mean, ' +- ', inc)
+
 
 
 ############################################################################################################################
 
                                 # PRUEBAS ETC:
 
-n = 4
-p = 0.3
+n = 5
+p = 0.2
 
 matriu = matriu_quadrat(n,p)
 cluster_vec = []
@@ -302,7 +461,7 @@ trobat = False
 for i in range(n):
     for j in range(n):
         if matriu[i,j] == 1 and not trobat:
-            cluster_vec = cluster_funcio(matriu, i, j, n,visitat)
+            #cluster_vec = cluster_funcio(matriu, i, j, n,visitat)
             trobat = True
     if trobat:
         break
@@ -322,4 +481,8 @@ else:
 
 print(fraccio_ver_con(matriu))
 print(biggest_cluster_frac(matriu))
+print('fracció del cluster que ha percolat')
+print(fracc_cluster_per(matriu))
+
+
 
